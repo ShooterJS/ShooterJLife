164949809df44c8357d391db1f7a150a00bfd9c8
prd-jiangshuai <prd-jiangshuai@winwayworld.com>
2021-11-23 11:23:23 +0800

    分布式ID生成，Hash算法，定时任务，分布式seesion共享

---
+++ shooterj-distributed/src/main/java/com/shooterj/idgen/UuidTest.java
@@ -0,0 +1,10 @@
+package com.shooterj.idgen;
+
+import java.util.UUID;
+
+public class UuidTest {
+    public static void main(String[] args) {
+        String uuid = UUID.randomUUID().toString();
+        System.out.println(uuid);
+    }
+}
